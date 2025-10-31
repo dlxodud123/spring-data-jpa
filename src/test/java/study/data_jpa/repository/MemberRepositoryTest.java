c5989315d2442fac7a21224ac949c94d5a1df2cf
LeeTaeYoung <xodud5080@naver.com>
2025-10-31 12:20:53 +0900

    param binding

--- src/test/java/study/data_jpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/data_jpa/repository/MemberRepositoryTest.java
@@ -9,6 +9,7 @@ import study.data_jpa.dto.MemberDto;
 import study.data_jpa.entity.Member;
 import study.data_jpa.entity.Team;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -127,6 +128,19 @@ public class MemberRepositoryTest {
         List<MemberDto> result = memberRepository.findMemberDto();
 
         for (MemberDto s : result) {
+            System.out.println("s = " + s);
+        }
+    }
+
+    @Test
+    public void findByNames(){
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findByNames(Arrays.asList("AAA", "BBB"));
+        for (Member s : result) {
             System.out.println("s = " + s);
         }
     }
